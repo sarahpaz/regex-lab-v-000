7507569df4997d56a3022e2ed6ceb5f7aae405e4
Sarah Paz <sarah.paz@gmail.com>
2018-12-27 20:32:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,5 @@
-def starts_with_a_vowel?(word)
-  word.match(/[aeiou]/i)
-  # if word[0].match(/[aeiou]/i)
-  #   true
-  # else
-  #   false
-  # end
+def starts_with_a_vowel?(word)  
+  word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
