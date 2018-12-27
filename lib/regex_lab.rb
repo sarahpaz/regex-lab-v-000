114d48cdfcc27933684b9e26dc988a656a4464cf
Sarah Paz <sarah.paz@gmail.com>
2018-12-27 20:54:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,6 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].+[[:punct:]]$/)  ? true : false
-  # text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
